bd6807ea218873df2bd0d833e003a14814c5a640
Álvaro Rocha <rochaamcl@net.sapo.pt>
2020-08-16 18:50:28 +0100

    pagina hora foto fundo e cor fundo

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel=78.2
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if ( vel>60) { //CONDIÇÃO SIMPLES
+    console.log (`Você ultrapassou a velocidade permitida. Multado`)
+}
+console.log("Dirija sempre utilizando o cinto de segurança.")
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var paìs="EUA"
+console.log(`Vivendo em ${paìs}`)
+if(paìs=="Brasil"){    // tambem podia ser assim: if(paìs!="Brasil")
+    console.log("Brasileiro") // console.log(" Voçê é estrangeiro ")
+} else{
+    console.log("Voçê é Estrangeiro")
+}
